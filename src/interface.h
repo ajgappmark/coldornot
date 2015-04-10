ced0f506745befb8b22d11b5af14e3c94cf50580
Philippe Loctaux <loctauxphilippe@gmail.com>
2015-04-10 23:48:20 +0200

    starting to split the program into multiple files

---
+++ src/interface.h
@@ -0,0 +1,26 @@
+/*
+ *
+ *	interface.h - Coldornot
+ *	Created by Philippe Loctaux
+ *
+ *
+ *	License - GPL v2
+ *
+ */
+
+
+int CtF();
+int FtC();
+int CtK();
+int KtC();
+int FtK();
+int KtF();
+int quit();
+
+
+/*** Algorithms ***/
+int CtFalgo();
+int FtCalgo();
+int KtCalgo();
+int FtKalgo();
+int KtFalgo();
---
+++ src/main.c
@@ -0,0 +1,24 @@
+/*
+ *
+ *	Coldornot -  A tool to convert temperatures.
+ *	Created by Philippe Loctaux
+ *
+ *	Version 1.0
+ *	License - GPL v2
+ *
+ */
+
+// Libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include "menu.h"
+#include "interface.h"
+
+int main(int argc, char const *argv[]) {
+    
+    printf("Welcome to Coldornot.\n\n");
+    menu();
+
+    return 0;
+}
+
