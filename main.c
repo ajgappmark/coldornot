f5b0345f9b25785140846f543ffddc1d74fc5678
Philippe Loctaux <loctauxphilippe@gmail.com>
2015-04-11 10:21:23 +0200

    ./configure needs main.c into the main directory

--- main.c
+++ main.c
@@ -11,8 +11,8 @@
 // Libraries
 #include <stdio.h>
 #include <stdlib.h>
-#include "menu.h"
-#include "interface.h"
+#include "src/menu.h"
+#include "src/interface.h"
 
 int main(int argc, char const *argv[]) {
     
